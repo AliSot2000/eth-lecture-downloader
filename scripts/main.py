af43ff887e94bd6eca96ae64fb0813556aa67fbc
AliSot2000 <alisot200@gmail.com>
2022-06-30 01:42:15 +0200

    + Full indexing function for the eth.

--- scripts/main.py
+++ scripts/main.py
@@ -1,30 +1,24 @@
 import datetime
-from eth_loader.site_indexer import ConcurrentETHSiteIndexer
+# from eth_loader.stream_loader import ConcurrentETHIndexer
+from eth_loader.indexer import ConcurrentETHIndexer
 from eth_loader.metadata_loader import EpisodeLoader
 from eth_loader.stream_loader import BetterStreamLoader, SpecLogin
-from secrets import user_name, password, spec_login
+
+# HandBrakeCLI
 
 if __name__ == "__main__":
-    path = "/home/alisot2000/Documents/01_ReposNCode/ETH-Lecture-Indexer/scripts/seq_sites.db"
-    index_start = datetime.datetime.now()
+    start = datetime.datetime.now()
     print("Started")
-    eid = ConcurrentETHSiteIndexer(path)
+    eid = ConcurrentETHIndexer("seq_sites.db")
     eid.index_video_eth()
     eid.gen_parent()
-    end = datetime.datetime.now()
-    print(f"required {(end - index_start).total_seconds()}s")
+    del eid
+    el = EpisodeLoader("seq_sites.db")
+    el.download()
+    del el
+    bsl = BetterStreamLoader("seq_sites.db", user_name="asotoudeh", password="3Xtv56HwjnpC5$Yx44PH!Tjt", spec_login=[SpecLogin(password="fG9LdsA", username="gro-21w", url="https://www.video.ethz.ch/lectures/d-infk/2021/autumn/252-0027-00L")])
+    bsl.initiator()
+    # bsl.deprecate_streams()
 
-    start = datetime.datetime.now()
-    print("Started")
-    eid = EpisodeLoader(path)
-    eid.download()
-    eid.deprecate(dt=index_start)
-    end = datetime.datetime.now()
-    print(f"required {(end - start).total_seconds()}s")
-
-    start = datetime.datetime.now()
-    print("Started")
-    eid = BetterStreamLoader(db=path, user_name=user_name, password=password, spec_login=spec_login)
-    eid.initiator()
     end = datetime.datetime.now()
     print(f"required {(end - start).total_seconds()}s")
