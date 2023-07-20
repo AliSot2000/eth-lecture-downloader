890a23cd8e7c9c9041d10d33110283e6d5f41cad
AliSot2000 <alisot200@gmail.com>
2023-07-21 01:00:13 +0200

    + Don't need session + Shortened imports

--- scripts/compressor.py
+++ scripts/compressor.py
@@ -1,14 +1,7 @@
-from concurrent.futures import ThreadPoolExecutor
-import threading as th
-from secrets import username, password, arguments, download_directory
-import datetime
+from secrets import arguments, download_directory
 from common import *
-
-
-start = datetime.datetime.now()
-session = rq.session()
 
 
 if __name__ == "__main__":
     compression_queue = build_args(dl_args=arguments, dl_dir=download_directory)
-    compress(compression_queue, 1, 2)
+    compress(compression_queue, 0, 1)
