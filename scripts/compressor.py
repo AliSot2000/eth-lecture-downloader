610ce5ea8c14b91470c050d3182c0d8c7f564143
AliSot2000 <alisot200@gmail.com>
2023-07-21 18:16:26 +0200

    + Only use CPU

--- scripts/compressor.py
+++ scripts/compressor.py
@@ -4,4 +4,4 @@ from common import *
 
 if __name__ == "__main__":
     compression_queue = build_args(dl_args=arguments, dl_dir=download_directory)
-    compress(compression_queue, 0, 1)
+    compress(compression_queue, 1, 0)
