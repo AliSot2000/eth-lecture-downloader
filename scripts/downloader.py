85f188ade1ba7da74c13636a919e9473ae63fb45
AliSot2000 <alisot200@gmail.com>
2024-01-09 00:45:03 +0100

    + Performance opt for Workstation

--- scripts/downloader.py
+++ scripts/downloader.py
@@ -3,4 +3,5 @@ from secrets import *
 
 if __name__ == "__main__":
     for arg in arguments:
-        download(arg, username, password, 2)
+        download(arg, username, password, 4)
+        # download(arg, username, password, 2)
