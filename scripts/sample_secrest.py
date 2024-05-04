bcb116ec2a81576b2318f2fb62ef0d60b5e9ae5e
AliSot2000 <alisot200@gmail.com>
2024-05-04 22:49:27 +0200

    + Updated sample_secrets.py to current status

--- scripts/sample_secrest.py
+++ scripts/sample_secrest.py
@@ -1,10 +1,12 @@
-from eth_loader.stream_loader import SpecLogin
+from common import SeriesArgs
 
 user_name = "<your username>"
 password = "<your password>"
 spec_login = [
     # url="https://www.video.ethz.ch/lectures/d-infk/2021/autumn/252-0027-00L"   # EXAMPLE
-    SpecLogin(password="<lecture password>",
+    SeriesArgs(password="<lecture password>",
               username="<lecture username>",
+               folder="<path to download>",
+                compressed_folder="<path to compressed download>",
               url="<url to lecture>")
 ]
